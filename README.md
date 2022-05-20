78a23baf7b07a1aff53bd4d144e3db04436338ce
marian douspis <marian.douspis@ias.u-psud.fr>
2022-05-20 14:19:06 +0200

    with kSZ spectra

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Highell_Cls
 
-Provide tools to compute  High ell angular power spectra of SZ related spectra. 
+provide tools to compute high multipole (small-scale) angular power spectra of the thermal and kinetic Sunyaev-Zel'dovich effects. Examples of how to obtain such spectra are given in the tutorial notebook.
 
-## tSZ Cls spectra
+## tSZ angular power spectra
 
-The tSZ Cls are computed thanks to a Random Forest trained network (see [Douspis et al. 2022](https://www.aanda.org/articles/aa/full_html/2022/03/aa42004-21/aa42004-21.html)).
+The tSZ Cls are computed with a Random Forest network (see Douspis et al. 2022).
 
 * Highell_Cls.Cl_tsz_fromRF(dictionary) returns ell, Cls between l=2, l=13500 with Cls in muK2 at 143GHz. The dictionary contains cosmological and scaling relation parameters such as: C_SR_params={"bias": 0.8,
                         "H0": 64,
@@ -13,25 +13,27 @@ The tSZ Cls are computed thanks to a Random Forest trained network (see [Douspis
                         "sig8": 0.8,
                         "alpha":1.77})
 
-* Highell_Cls.Cl_tsz_fromRF(dictionary) uses two input files corresponding to the coefficients of the Random Forest (RF-TSZ-2021-3__clf_rf_params.pickle) and the exponents of the parameters dependency (tRF-TSZ-2021-4__exponents.pickle) stored in tszcl_coeff/. If the RF-params is not already in your directory (first time try) it will be downloaded. 
+* Highell_Cls.Cl_tsz_fromRF(dictionary) uses two input files corresponding to the coefficients of the Random Forest (RF-TSZ-2021-3__clf_rf_params.pickle) and the exponents of the parameters dependency (tRF-TSZ-2021-4__exponents.pickle) stored in tszcl_coeff/ If the RF-params is not already in your directory (first timetry) t will be downloaded. 
 
-## Usage
+## kSZ angular power spectra
 
-The Highell_Cls.ypnb file allows you to see how to use the code with a few examples.
+The late-time and patchy kSZ Cls are computed independently with a Random Forest network, given a set of cosmological and reionisation parameters(see Gorce et al. 2022 for more details).
 
-An interactive interface allows you to compute on the fly your prefered tSZ spectrum model at the [IAS SZ Database portal](https://szdb.osups.universite-paris-saclay.fr/)
-
-
+* Highell_Cls.Cl_ksz_fromRF(dictionary) returns the ell-bins the RF was trained on (between l=2, l=13500), the late-time kSZ, and the patchy kSZ Cls in muK2. The dictionary contains cosmological and sreionisation parameters such as: 
+param_dict={"omegabh2": 0.0224,
+            "omegach2": 0.120,
+            "ns": 0.9665,
+            "theta": 1.041,
+            "logA": 3.044,
+            "zre": 7.5,
+            "dz": 1.2,
+            "logalpha0": 3.7,
+            "kappa": 0.10}
 
 ## Licence
 
-This project is licensed under the Common Creative CC BY-NC. DOI: https://doi.org/10.48326/idoc.cosmo.ml-sz-aps
+This project is licensed under the LGPL+3.0 licence.
 
 ## Acknowledgements
 
-Please acknowledge the use of the code by citing: [Douspis, Salvati, Gorce, Aghanim, A&A, 659, A99, 2022](https://www.aanda.org/articles/aa/full_html/2022/03/aa42004-21/aa42004-21.html) and   
-adding the following in the acknowledgements: "This work (DOI: https://doi.org/10.48326/idoc.cosmo.ml-sz-aps) used the RF-High-ell spectra python code available at szdb.osups.universite-paris-saclay.fr"
-
-## Next
-
-Coming soon, the kSZ spectrum computation based on [Gorce et al. 2020](https://www.aanda.org/articles/aa/full_html/2020/08/aa38170-20/aa38170-20.html) and [Gorce et al. 2022](https://arxiv.org/abs/2202.08698)
+Please acknowledge the use of the code by citing: Douspis, Salvati, Gorce, Aghanim, A&A, 659, A99, 2022 and Gorce, Douspis, Salvati, A&A 2022, as well as adding the following in the acknowledgements: "This work used the RF-High-ell spectra python code available at szdb.osups.universite-paris-saclay.fr". 
--- tszcl_src/Highell_Cls.py
+++ tszcl_src/Highell_Cls.py
@@ -1,20 +1,31 @@
 # Copyright (c) 2022 IAS / CNRS / Univ. Paris-Saclay
-# CC BY-NC License 
-# Author: Marian Douspis <marian.douspis@ias.u-psud.fr>
-# DOI: https://doi.org/10.48326/idoc.cosmo.ml-sz-aps
+# LGPL License - see attached LICENSE file
+# Authors: Marian Douspis <marian.douspis@ias.u-psud.fr>
+#          Adélie Gorce <adelie.gorce@gmail.com>
 
+import pickle
+import sklearn
+import numpy as np
+import warnings
+import os
+import scipy as sp
+import requests
 
 def Cl_tsz_fromRF(C_SR_params={"bias": 0.8,
-                        "H0": 64,
-                        "omb": 0.05,
-                        "om": 0.31,
-                        "sig8": 0.8,
-                               "alpha":1.77}, coeff_path="../tszcl_coeff/"):
+                               "H0": 64,
+                               "omb": 0.05,
+                               "om": 0.31,
+                               "sig8": 0.8,
+                               "alpha":1.77},
+                  coeff_path="../tszcl_coeff/"):
     
     '''Return ell, Cl_TSZ from a set of cosmological and scaling relation parameters
 
-    C_SR_params: Cosmological and Scaling Relation parameters
-                 Dictionnary containing the following keys:
+        Parameters
+        ----------
+        C_SR_params: Dictionary
+                Cosmological and Scaling Relation parameters gathered in a 
+                dictionary containing the following keys:
                  H0   : Hubble constant in [60,80]
                  omb  : Omega_baryon in [0.02,0.07]
                  om   : Ommega_matter in [0.2, 0.4]
@@ -29,34 +40,37 @@ def Cl_tsz_fromRF(C_SR_params={"bias": 0.8,
                                  "sig8": 0.8,
                                  "alpha":1.77
                                   }
-    output: ell in [2,13500], Cl_tSZ in muK2 at 143GHz
+      coeff_path: string
+               Path to folder containing the RF coefficient file.
+               
+      Returns
+      ----------
+      ells: list of floats
+          List of angular multipoles the power spectrum has been 
+          calculated at (ell in [2,13500])
+      Cl_tSZ: list of floats
+          List of angular power spectrum values at 143GHz in muK2
 
     '''
 
-    import pickle
-    import numpy as np
-    import scipy as sp
-    import requests
-    import os.path
+    # tests
+    assert isinstance(C_SR_params, dict), \
+        "Must feed cosmological parameters as dictionary - see docstring."
+    assert isinstance(coeff_path, str), \
+        "Must feed path to RF coefficients as a string"
 
-    # December 2021
-    seed_coeff     =  "RF-TSZ-2021-3"
-    seed_exponents =  "RF-TSZ-2021-4"
-
-
-    file4coeff = coeff_path+seed_coeff+"__clf_rf_params.pickle"
-    
+    file4coeff = "{}RF-TSZ-2021-3__clf_rf_params.pickle".format(coeff_path)
     if not os.path.exists(file4coeff):
         url = 'https://www.ias.u-psud.fr/douspis/RF-TSZ-2021-3__clf_rf_params_dld.pickle'
         r = requests.get(url, allow_redirects=True)
-
         open(file4coeff, 'wb').write(r.content)
-    
-    
-    rf             = pickle.load(
-        open(file4coeff, 'rb'))
-    expo           = pickle.load(
-        open(coeff_path+seed_exponents+"__exponents.pickle", 'rb'))
+        warnings.warn('Retrieving RF files from {}'.format(url))
+    rf             = pickle.load(open(file4coeff, 'rb'))
+
+    file4expo = "{}RF-TSZ-2021-4__exponents.pickle".format(coeff_path)
+    assert os.path.exists(file4expo), \
+        "File with exponents missing"
+    expo           = pickle.load(open(file4expo, 'rb'))
 
     paramsref = [0.80E+00,
                  -0.2088383E+00,
@@ -102,3 +116,112 @@ def Cl_tsz_fromRF(C_SR_params={"bias": 0.8,
                                * params[9]**expo[10])
                       
     return new_xx, output2
+
+def Cl_ksz_fromRF(param_dict={"omegabh2": 0.0224,
+                              "omegach2": 0.120,
+                              "ns": 0.9665,
+                              "theta": 1.041,
+                              "logA": 3.044,
+                              "zre": 7.5,
+                              "dz": 1.2,
+                              "logalpha0": 3.7,
+                              "kappa": 0.10},
+                  coeff_path="./kszcl_coeff/"):
+    
+    '''Return ell, Cl_KSZ from a set of cosmological and scaling relation parameters
+
+      Parameters
+      ----------
+      param_dict: Dictionary
+                  Cosmological and reionisation parameters gathered in a 
+                  dictionary containing the following keys:
+                     omegabh2   : Density of baryons at z=0 scaled by h^2
+                     omegach2  :Density of cold DM at z=0 scaled by h^2
+                     ns   : Scalar spectral index
+                     theta : Ratio of the sound horizon to the angular diameter distance 
+                             at decoupling, scaled by 100
+                     logA : ln(10^10 As) for As the amplitude of initial perturbations
+                     zre : Midpoint of reionisation
+                     dz : zre-zend, where zend is the endpoint of reionisation
+                     logalpha0 : decimal logarithm of the large-scale amplitude of the 
+                              electron power spectrum
+                     kappa : drop-off frequency of the power spectrum of free electrons 
+                             density fluctuations
+      coeff_path: string
+               Path to folder containing the RF coefficient file.
+               
+      Returns
+      ----------
+      ells: 1D array of floats
+          List of angular multipoles the power spectrum has been 
+          calculated at (ell in [2,13500]).
+      Cl_kSZ late-time: array of floats
+          Late-time kSZ angular power spectrum values in muK2.
+      Cl_kSZ patchy: array of floats
+          Patchy kSZ angular power spectrum values in muK2.
+
+    '''
+    
+    # tests
+    assert isinstance(param_dict, dict), \
+        "Must feed cosmological parameters as dictionary - see docstring."
+    assert isinstance(coeff_path, str), \
+        "Must feed path to RF coefficients as a string"
+    assert os.path.exists("{}RF-KSZ_ells.txt".format(coeff_path)), \
+        "File necessary to read RF missing"
+
+
+
+    file4coeff = "{}RF-KSZ_patchy_cl_rf.pickle".format(coeff_path)
+    if not os.path.exists(file4coeff):
+        url = 'https://www.ias.u-psud.fr/douspis/RF-KSZ_patchy_cl_rf.pickle'
+        r = requests.get(url, allow_redirects=True)
+        open(file4coeff, 'wb').write(r.content)
+        warnings.warn('Retrieving RF files patchy from {}'.format(url))
+
+    file4coeff = "{}RF-KSZ_late_cl_rf.pickle".format(coeff_path)
+    if not os.path.exists(file4coeff):
+        url = 'https://www.ias.u-psud.fr/douspis/RF-KSZ_late_cl_rf.pickle'
+        r = requests.get(url, allow_redirects=True)
+        open(file4coeff, 'wb').write(r.content)
+        warnings.warn('Retrieving RF files late from {}'.format(url))
+
+
+    
+    # convert parameter dictionary to array
+    paramnames = ['omegabh2', 'omegach2', 'ns', 'theta', 'logA',
+                  'zre', 'dz', 'logalpha0', 'kappa']
+    paramlims = [(0.0200,0.0300), (0.100,0.150), (0.957,0.990), (1.03,1.05), (2.8,3.3),
+                 (5. ,10.), (0.1,5.), (2.,4.5), (0.04,0.20)]
+    
+    ndeg = len(paramnames)
+    params = np.zeros(ndeg)
+    for u, param in enumerate(paramnames):
+        assert param in list(param_dict.keys()), \
+            "Missing parameter in dictionary: {}".format(param)
+        params[u] = float(param_dict[param])
+        if not (paramlims[u][0] <= params[u] <= paramlims[u][1]):
+            warnings.warn('Value of parameter {} outside of training range.'.format(param))
+            
+    # Read files
+    # Angular multipoles the RF has been trained on
+    ll = np.loadtxt("{}RF-KSZ_ells.txt".format(coeff_path))
+    # Patchy KSZ coefficients
+    rfp = pickle.load(open("{}RF-KSZ_patchy_cl_rf.pickle".format(coeff_path), 'rb')) 
+    expop, params_ref = np.loadtxt("{}RF-KSZ_patchy_exponents.txt".format(coeff_path), unpack=True)
+    # Late-time KSZ coefficients
+    rfh = pickle.load(open("{}RF-KSZ_late_cl_rf.pickle".format(coeff_path), 'rb')) 
+    expoh, params_ref = np.loadtxt("{}RF-KSZ_late_exponents.txt".format(coeff_path), unpack=True)
+
+    # patchy
+    pksz = rfp.predict(params.reshape(1, ndeg)).flatten()
+    pksz = pksz*expop[0]*np.product(np.abs(params/params_ref[1:])**expop[1:])
+    pksz[pksz<0] = 0.
+    
+    # late-time
+    hksz = rfh.predict(params.reshape(1, ndeg)).flatten()
+    hksz = hksz*expoh[0]*np.product(np.abs(params/params_ref[1:])**expoh[1:])
+    hksz[hksz<0] = 0.
+
+    return ll, hksz, pksz
+
